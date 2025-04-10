c11e5e0582b60ad08352cd2e40c3a9a5df79aba3
zyaireD <zyaire.dowe25@compscihigh.org>
2025-04-10 17:29:15 +0000

    done

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
 // Create a function that tests the post route handler [2 pts]
 
-
-
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
 
+async function test ( , data){
+  const response= await fetch({
+    method:"POST",
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify(potionSchema.label.effect.ingredients.color.isexplosive)
+})
+const data= await response.json
+
+}
+
+test()
